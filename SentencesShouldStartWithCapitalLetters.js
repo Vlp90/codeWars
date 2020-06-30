2a7e45fce7e365be29fe219dae1b09d97b23b378
Vlp90 <vladimir.leloup@gmail.com>
2020-06-30 17:24:40 +0200

    new exercice

---
+++ SentencesShouldStartWithCapitalLetters.js
@@ -0,0 +1,20 @@
+// In English, all words at the begining of a sentence should begin with a capital letter.
+
+// You will be given a paragraph that does not use capital letters. Your job is to capitalise the first letter of the first word of each sentence.
+
+// For example,
+
+// input:
+
+// "hello. my name is inigo montoya. you killed my father. prepare to die."
+
+// output:
+
+// "Hello. My name is inigo montoya. You killed my father. Prepare to die."
+
+
+const fix = (paragraph) => {
+return paragraph
+}
+
+console.log(fix('hello. my name is inigo montoya. you killed my father. prepare to die.'))
