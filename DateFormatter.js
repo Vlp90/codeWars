c58a59f868159a3be030a347a7a65a99a65a0e45
Vlp90 <vladimir.leloup@gmail.com>
2020-07-06 12:00:04 +0200

    new exercice

---
+++ DateFormatter.js
@@ -0,0 +1,13 @@
+// You are to implement a getDate function, which takes a unix timestamp and a locale as
+// input and returns the formatted date based on the locale.
+
+// the locale formats to use are stored in the
+// preloaded structure formats. For example,
+
+// Example:
+
+// getDate(1481020142, 'en-GB') -> '06/12/2016' (format->'dd/mm/yyyy')
+// getDate(1481020142, 'de-DE') -> '06.12.2016' (format->'dd.mm.yyyy')
+// getDate(1481020142, 'hu-HU') -> '2016. 12. 06.' (format->'yyyy. mm. dd.')
+
+const getDate = (uts, loc) => {};
