58250ef04768b4aa9964e2c353f813f45c576992
Vlp90 <vladimir.leloup@gmail.com>
2020-07-06 13:06:53 +0200

    exercice done

--- DateFormatter.js
+++ DateFormatter.js
@@ -10,6 +10,31 @@
 // getDate(1481020142, 'de-DE') -> '06.12.2016' (format->'dd.mm.yyyy')
 // getDate(1481020142, 'hu-HU') -> '2016. 12. 06.' (format->'yyyy. mm. dd.')
 
-const getDate = (uts, loc) => {
-    
-};
+let getDate = (uts, loc) => require('moment').unix(uts).format((formats[loc] || 'DD/MM/YYYY').toUpperCase());
+
+
+// const getDate = (uts, loc) => {
+
+
+
+//     let date = new Date(uts * 1000);
+//     let day = date.getDate();
+//     let month = date.getMonth() + 1;
+//     var year = date.getFullYear();
+  
+//     if (loc === "en-GB") {
+//       // console.log(day);
+//       // console.log(month);
+//       // console.log(year);
+//       return 0+ `${day}/${month}/${year}`;
+//     } else if (loc === "de-DE") {
+//       return 0+`${day}.${month}.${year}`;
+//     } else if (loc === "en-US") {
+//       return `${month}/${day}/${year}`;
+//     }
+//   };
+  
+  
+  console.log(getDate(1481020142, "en-GB"));
+  console.log(getDate(1481020142, "de-DE"));
+  console.log(getDate(1481020142, "en-US"));
