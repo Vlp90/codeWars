08b70a7a8b73b8e1249b95dac20913a0e0cbe9c5
Vlp90 <vladimir.leloup@gmail.com>
2020-08-11 10:57:17 +0200

    exercice done

--- Fix-string-case.js
+++ Fix-string-case.js
@@ -1,4 +1,5 @@
-// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is
+// to convert that string to either lowercase only or uppercase only based on:
 
 // make as few changes as possible.
 // if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
@@ -7,3 +8,31 @@
 // solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
 // solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
 // solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s) {
+  let upperCaseCount = 0;
+  let lowerCaseCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    //   console.log(s[i])
+
+    let letter = s[i];
+    if (letter === letter.toUpperCase()) {
+      upperCaseCount += 1;
+    } else {
+      lowerCaseCount += 1;
+    }
+  }
+  console.log(upperCaseCount);
+  console.log(lowerCaseCount);
+
+  if (upperCaseCount > lowerCaseCount) {
+    return s.toUpperCase();
+  } else if (upperCaseCount < lowerCaseCount) {
+    return s.toLowerCase();
+  } else if (upperCaseCount === lowerCaseCount) {
+    return s.toLowerCase();
+  }
+}
+
+console.log(solve("CAZe"));
