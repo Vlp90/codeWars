0d19247f3364284cbcb9ab425a6e41dd1a3260f0
Vlp90 <vladimir.leloup@gmail.com>
2020-10-05 17:23:43 +0200

    done

--- sum-of-pairs.js
+++ sum-of-pairs.js
@@ -1,2 +1,19 @@
-// Given a list of integers and a single sum value, 
+// Given a list of integers and a single sum value,
 // return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+const sum_pairs = (ints, s) => {
+    // const { length } = ints;
+
+  for (let i = -1; i < ints.length; i++) {
+      console.log("ints[i]", ints[i])
+    for (let j = 0; j < i; j++) {
+        console.log("ints[j]",ints[j])
+      if (ints[i] + ints[j] === s) {
+          return [ints[j], ints[i]];
+        }
+    }
+  }
+
+};
+
+console.log(sum_pairs([10,5,2,3,7,5], 10));
