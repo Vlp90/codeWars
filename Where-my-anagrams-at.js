1140048caa1216e9b31a34f1aea9bfb0e720a391
Vlp90 <vladimir.leloup@gmail.com>
2020-10-07 16:32:00 +0200

    done

--- Where-my-anagrams-at.js
+++ Where-my-anagrams-at.js
@@ -4,10 +4,32 @@
 // 'abba' & 'abbba' == false
 // 'abba' & 'abca' == false
 
-// Write a function that will find all the anagrams of a word from a list. 
-// You will be given two inputs a word and an array with words. 
+// Write a function that will find all the anagrams of a word from a list.
+// You will be given two inputs a word and an array with words.
 // You should return an array of all the anagrams or an empty array if there are none. For example:
 
 // anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
 // anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
 // anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+
+const anagrams = (word, words) => {
+  let orderWord = word.split("").sort().join("");
+  let result = [];
+
+  for (const element of words) {
+    // console.log(element);
+
+    if (element.split("").sort().join("") == orderWord) {
+      result.push(element);
+    } else {
+        result.push()
+    }
+  }
+
+  return result;
+};
+
+console.log(anagrams("abba", ["aabb", "abcd", "bbaa", "dada"]));
+console.log(anagrams('laser', ['lazing', 'lazy',  'lacer']));
+
