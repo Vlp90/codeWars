ac2cbc4ee0f539aa0688f09d35f1765f687bb8aa
Vlp90 <vladimir.leloup@gmail.com>
2020-10-08 15:06:24 +0200

    done

--- array-addition.js
+++ array-addition.js
@@ -3,3 +3,23 @@
 // additionnant deux éléments distincts du tableau.
 // L'optimisation est le point intéressant du problème.
 // [1, 1, 2, 3, 5, 6, 12, 13, 15, 15, 16, 18, 20, 27, 29]
+
+const sumPairs = (array, value) => {
+  let answer = [];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[i] + array[j] === value) {
+        let result = [array[i], array[j]];
+        answer.push(result);
+      }
+    }
+  }
+  //   return answer
+  final = answer.join(" et ");
+  return `L'entier ${value} peut être optenu en additionant les pairs suivantes : ${final}`;
+};
+
+console.log(
+  sumPairs([1, 1, 2, 3, 5, 6, 12, 13, 15, 15, 16, 18, 20, 27, 29], 8)
+);
