8f8ed3e97034ab975f97f470788bb9c08e1ef1d7
Vlp90 <vladimir.leloup@gmail.com>
2020-10-08 17:17:15 +0200

    done

--- Name-or-World.js
+++ Name-or-World.js
@@ -10,3 +10,28 @@
 // hello "aliCE"  => "Hello, Alice!"
 // hello          => "Hello, World!" # name not given
 // hello ''       => "Hello, World!" # name is an empty String
+
+
+// const hello = (name) => {
+
+// if (name === '' || name === undefined) {
+//     return "Hello, World!"
+// } else {
+//     let firstLetterUpperCase = name[0].toUpperCase()
+//     let resteName = name.slice(1, name.length).toLowerCase()
+//     let result = firstLetterUpperCase + resteName 
+    
+// return `Hello, ${result}!`
+//  }
+
+// }
+
+// Other Answer
+const hello = (name) => {
+return (name === '' || name === undefined) ? 'Hello, World!' : `Hello, ${name.charAt(0).toUpperCase() + name.toLowerCase().substr(1)}!`;
+
+}
+
+console.log(hello("johN"))
+console.log(hello("alice"))
+console.log(hello())
