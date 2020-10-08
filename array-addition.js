8221c193b7b4be8e740e72a065d4f9e21141cd1f
Vlp90 <vladimir.leloup@gmail.com>
2020-10-08 13:59:07 +0200

    new ex

---
+++ array-addition.js
@@ -0,0 +1,5 @@
+// Test 2 :
+// Créer un programme qui permettra de déterminer si on peut obtenir l'entier 8 en
+// additionnant deux éléments distincts du tableau.
+// L'optimisation est le point intéressant du problème.
+// [1, 1, 2, 3, 5, 6, 12, 13, 15, 15, 16, 18, 20, 27, 29]
