7df6bdba36063ed6cfd34792ba5d8d8968e0deab
Vlp90 <vladimir.leloup@gmail.com>
2020-10-09 12:48:40 +0200

    done

--- Check-the-exam.js
+++ Check-the-exam.js
@@ -1,7 +1,7 @@
 // The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
 
-// The two arrays are not empty and are the same length. Return the score for this array of answers, 
-// giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, 
+// The two arrays are not empty and are the same length. Return the score for this array of answers,
+// giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer,
 // represented as an empty string (in C the space character is used).
 
 // If the score < 0, return 0.
@@ -12,3 +12,24 @@
 // checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
 // checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
 // checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+const checkExam = (array1, array2) => {
+  let score = 0;
+
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] === array2[x]) {
+      score += 4;
+    } else if (array2[x] === "") {
+      score += 0;
+    } else {
+      score -= 1;
+    }
+  }
+
+  if (score < 0) {
+    score = 0;
+  }
+  return score;
+};
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
