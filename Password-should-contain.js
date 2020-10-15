f4e390f5c83661a4d88e02460dfa6edfb4ad8a12
Vlp90 <vladimir.leloup@gmail.com>
2020-10-15 11:00:47 +0200

    done

--- Password-should-contain.js
+++ Password-should-contain.js
@@ -1,19 +1,31 @@
-// You are writing a password validator for a website. 
-// You want to discourage users from using their username as part of their password, or vice-versa, 
-// because it is insecure. Since it is unreasonable to simply not allow them to have any letters in 
+// You are writing a password validator for a website.
+// You want to discourage users from using their username as part of their password, or vice-versa,
+// because it is insecure. Since it is unreasonable to simply not allow them to have any letters in
 // common, you come up with this rule:
 
-// For any password and username pair, the length of the longest common substring should be less 
+// For any password and username pair, the length of the longest common substring should be less
 // than half the length of the shortest of the two.
 
-// In other words, you won't allow users to have half their password repeated in their username, 
+// In other words, you won't allow users to have half their password repeated in their username,
 // or half their username repeated in their password.
 
-// Write a function validate(username, password) which returns true if your rule is followed, 
+// Write a function validate(username, password) which returns true if your rule is followed,
 // false otherwise.
 
 // Assume:
 
-// Both the username and the password may contain uppercase letters, lowercase letters, 
+// Both the username and the password may contain uppercase letters, lowercase letters,
 // numbers, spaces, and the following special/punctation characters: !@#$%^&*()_+{}:"<>?[];',.
 // The username and password will each be less than 100 characters.
+
+const validate = (username, password) => {
+    const half = Math.ceil(Math.min(username.length, password.length) / 2);
+    for (let i = 0; i <= password.length - half; i++) {
+      if (username.includes(password.substr(i, half))) return false;
+    }
+    return true;
+  };
+
+console.log(validate("sword", "password"));
+console.log(validate("qwertyuiop", "asdfghjkl"));
+console.log(validate("MASH", "M*A*S*H"));
